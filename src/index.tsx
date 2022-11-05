279b7648ef035c3555a849e02f7b8f7aa9bd41c3
RiRyCasals <rirycasals@gmail.com>
2022-11-05 10:07:41 +0900

    p83まで終了

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Hello from './components/Hello';
 import reportWebVitals from './reportWebVitals';
+import Name from './components/Name';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Hello/>
+    <Name/>
   </React.StrictMode>
 );
 
