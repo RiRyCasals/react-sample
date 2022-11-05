4201cb5ab1e274a73660979b7e1ff4b4a5e25e75
RiRyCasals <rirycasals@gmail.com>
2022-11-05 14:55:30 +0900

    p92まで終了

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import Name from './components/Name';
 import Message from './components/Message';
 import Parent from './components/ContainerSample';
 import Page from './components/ContextSample';
+import Counter from './components/Counter';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -18,6 +19,7 @@ root.render(
     <Message/>
     <Parent/>
     <Page/>
+    <Counter initialValue={0}/>
   </React.StrictMode>
 );
 
