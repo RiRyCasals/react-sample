d62e4edf63529d8d3760562103bc8511b97359e7
RiRyCasals <rirycasals@gmail.com>
2022-11-05 14:07:11 +0900

    p86まで終了

--- src/components/ContainerSample.tsx
+++ src/components/ContainerSample.tsx
@@ -1,4 +1,9 @@
-const Container = (props: { title: string; children: React.ReactElement}) => {
+type ContainerProps = {
+    title: string,
+    children: React.ReactNode
+}
+
+const Container = (props: ContainerProps): JSX.Element => {
     const { title, children } = props
     return (
         <div style={{background: 'red'}}>
@@ -8,7 +13,7 @@ const Container = (props: { title: string; children: React.ReactElement}) => {
     )
 }
 
-const Parent = () => {
+const Parent = (): JSX.Element => {
     return (
         <Container title="Hello">
             <p>ここの部分が背景色で囲まれます</p>
